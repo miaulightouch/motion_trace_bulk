1dc9fb378eb8ab0d3264be8eb42d0eb15d20fecc
errno-mmd <errno.mmd2@gmail.com>
2019-05-06 03:06:00 +0900

    過去深度データ読み込み処理追加(Docker版)

--- Docker/BulkVmd_Docker.bat
+++ Docker/BulkVmd_Docker.bat
@@ -8,7 +8,7 @@ echo VMD-3d-pose-baseline-multi [%IDX%]
 echo ------------------------------------------
 
 rem ---  python é¿çs
-set P2V_ARG=-v %VERBOSE% -t "%C_OUTPUT_SUB_DIR%" -b "born/animasa_miku_born.csv" -c 30 -z 5 -s 1 -p 0.5 -r 3 -k 1 -e 0
+set P2V_ARG=-v %VERBOSE% -t "%C_OUTPUT_SUB_DIR%" -b "born/animasa_miku_born.csv" -c 30 -z 0 -s 1 -p 0.5 -r 3 -k 1 -e 0
 docker container run --rm -v %INPUT_VIDEO_DIR:\=/%:/data -it errnommd/autotracevmd:%IMAGE_TAG% bash -c "cd /VMD-3d-pose-baseline-multi && python3 applications/pos2vmd_multi.py %P2V_ARG%"
 
 exit /b
